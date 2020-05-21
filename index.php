3eee7ee4b7cce95b3afa5e0fe0c1e65d4889d350
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-05-21 13:30:04 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-  // $db = new mysqli('localhost','root','','magnito_dashboard');//set your database handler
   include 'config.php';
 
   $sql1 = "SELECT id,dept_name FROM departments";
   $query1 = $dbh->prepare($sql1);
   $query1->execute();
-  // $result = $db->query($query);
   $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
 
   if ($query1->rowCount() > 0) {
@@ -14,30 +12,19 @@
     }
   }
 
-  // while($row = $result->fetch_assoc()){
-  //   $categories[] = array("id" => $row['id'], "val" => $row['dept_name']);
-  // }
-
   $sql2 = "SELECT id, deptid, catname FROM task_categories";
   $query2 = $dbh->prepare($sql2);
   $query2->execute();
-  // $result = $db->query($query);
   $results2 = $query2->fetchAll(PDO::FETCH_OBJ);
 
   if ($query2->rowCount() > 0) {
     foreach ($results2 as $result2) {
       $subcats[$result2->deptid][] = array("id" => $result2->id, "val" => $result2->catname);
-      // $categories[] = array("id" => $result1->id, "val" => $result1->dept_name);
     }
   }
 
-  // while($row = $result->fetch_assoc()){
-  //   $subcats[$row['deptid']][] = array("id" => $row['id'], "val" => $row['catname']);
-  // }
-
   $jsonCats = json_encode($categories);
   $jsonSubCats = json_encode($subcats);
-
 ?>
 
 <!docytpe html>
