f38392e0b85a57ebc1ba781c7db2de466944ff7f
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-05-21 12:56:13 +0600

    pdo implemented

--- index.php
+++ index.php
@@ -1,19 +1,41 @@
 <?php
-  $db = new mysqli('localhost','root','','magnito_dashboard');//set your database handler
-  
-  $query = "SELECT id,dept_name FROM departments";
-  $result = $db->query($query);
+  // $db = new mysqli('localhost','root','','magnito_dashboard');//set your database handler
+  include 'config.php';
 
-  while($row = $result->fetch_assoc()){
-    $categories[] = array("id" => $row['id'], "val" => $row['dept_name']);
+
+
+  $sql1 = "SELECT id,dept_name FROM departments";
+  $query1 = $dbh->prepare($sql1);
+  $query1->execute();
+  // $result = $db->query($query);
+  $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+
+  if ($query1->rowCount() > 0) {
+    foreach ($results1 as $result1) {
+      $categories[] = array("id" => $result1->id, "val" => $result1->dept_name);
+    }
   }
 
-  $query = "SELECT id, deptid, catname FROM task_categories";
-  $result = $db->query($query);
+  // while($row = $result->fetch_assoc()){
+  //   $categories[] = array("id" => $row['id'], "val" => $row['dept_name']);
+  // }
 
-  while($row = $result->fetch_assoc()){
-    $subcats[$row['deptid']][] = array("id" => $row['id'], "val" => $row['catname']);
+  $sql2 = "SELECT id, deptid, catname FROM task_categories";
+  $query2 = $dbh->prepare($sql2);
+  $query2->execute();
+  // $result = $db->query($query);
+  $results2 = $query2->fetchAll(PDO::FETCH_OBJ);
+
+  if ($query2->rowCount() > 0) {
+    foreach ($results2 as $result2) {
+      $subcats[$result2->deptid][] = array("id" => $result2->id, "val" => $result2->catname);
+      // $categories[] = array("id" => $result1->id, "val" => $result1->dept_name);
+    }
   }
+
+  // while($row = $result->fetch_assoc()){
+  //   $subcats[$row['deptid']][] = array("id" => $row['id'], "val" => $row['catname']);
+  // }
 
   $jsonCats = json_encode($categories);
   $jsonSubCats = json_encode($subcats);
